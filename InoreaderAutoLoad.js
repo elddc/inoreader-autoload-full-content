867b8e9e794559b1468fd5d5b4c61aaab6208360
Emma Chen <78119615+elddc@users.noreply.github.com>
2023-01-08 17:18:22 -0600

    Fix with new selectors

--- InoreaderAutoLoad.js
+++ InoreaderAutoLoad.js
@@ -1,36 +1,33 @@
 // ==UserScript==
 // @name         Inoreader Auto-Load Full Article
 // @namespace    elddc
-// @version      1.0
+// @version      1.1
 // @description  Automatically load full content when opening an article in Inoreader
 // @author       Elddc
 // @match        https://www.inoreader.com/*
 // @grant        none
 // ==/UserScript==
 
-/* jshint esversion: 6 */
-(function () {
-	//----- configure (change as desired) -----
-	const excludedSources = ['The Associated Press']; //subscriptions to exclude from auto-load
-	const waitTime = 200; //time in ms to wait for page load (1 sec: 1000ms)
-	//-----------------------------------------
+//----- configure (change as desired) -----
+const excludedSources = ['The Associated Press']; //subscriptions to exclude from auto-load
+const waitTime = 200; //time in ms to wait for page load (1 sec: 1000ms)
+//-----------------------------------------
 
-	loadFullArticle();
-	window.onpopstate = loadFullArticle;
+loadFullArticle();
+window.onpopstate = loadFullArticle;
 
-	function loadFullArticle() {
-		if (window.location.pathname.includes('article/')) {
-			setTimeout(() => {
-				try {
-					const source = document.getElementsByClassName('boldlink')[0];
-					const button = document.getElementsByClassName('icon-button-mobilize-empty')[0];
-					if (!excludedSources.includes(source.innerHTML.trim())) {
-						button.click();
-					}
-				} catch (err) {
-					console.log('page has not loaded');
-				}
-			}, waitTime);
-		}
-	}
-})();
+function loadFullArticle() {
+    if (window.location.pathname.includes('article/')) {
+        setTimeout(() => {
+            try {
+                const source = document.querySelector('.article_subtitle_author_wrapper');
+                const button = document.querySelector('.icon-article_topbar_mobilize_empty');
+                if (!excludedSources.includes(source.innerHTML.trim())) {
+                    button.click();
+                }
+            } catch (err) {
+                console.log('page has not loaded');
+            }
+        }, waitTime);
+    }
+}
